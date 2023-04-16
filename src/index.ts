34188d5f74cc6c7d5082717cedcf2e83ffc8b20f
Gabriel de Oliveira Silva <gabriel.oliveira.s177@gmail.com>
2023-04-16 18:02:00 -0400

    testing

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,17 @@
-import express, { Request, Response, Router } from "express";
-import serverless from "serverless-http";
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 // Create an instance of the Express app
 const app = express();
 
-// Create a router to handle routes
-const router = express.Router();
-
-// Define a route that responds with a JSON object when a GET request is made to the root path
-router.get("/", (req, res) => {
+app.get("/", (req, res) => {
   res.json({
-    hello: "hi!",
+    deu: "certo",
   });
 });
 
-// Use the router to handle requests to the `/.netlify/functions/api` path
-app.use(`/.netlify/functions/api`, router);
+const PORT = process.env.PORT || 3000;
 
-// Export the app and the serverless function
-export default app;
-export const handler = serverless(app);
+app.listen(PORT, () => console.log(`Server running on port ${3000}`));
